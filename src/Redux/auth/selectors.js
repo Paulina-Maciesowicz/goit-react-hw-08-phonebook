3d4137547f22136c834a257685a7f7c888f071a2
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-07-23 22:20:56 +0200

    update
    update

--- src/Redux/auth/selectors.js
+++ src/Redux/auth/selectors.js
@@ -1,5 +1,5 @@
-export const selectIsLoggedIn = state => true;
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 
-export const selectUser = state => "user";
+export const selectUser = state => state.auth.user;
 
-export const selectIsRefreshing = state => false;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
